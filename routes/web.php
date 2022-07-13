8035cb8102625bc3ca4de200cb876c54e1c1edd7
PhanHongHuynh <huynhph141100@gmail.com>
2022-07-13 13:48:14 +0700

    Intership

--- routes/web.php
+++ routes/web.php
@@ -13,12 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::prefix('admin')->group(function(){
-    Route::get('/login', function () {
-        return view('login');
-    });
-    Route::post('/user/create', [CreateUserController::class, 'validationForm']);
-    Route::get('/createuser', function () {
-        return view('createuser');
-    });
+Route::get('/createuser', function () {
+    return view('createuser');
 });
