7e832492d5e0b755ddd1d546d8ae3f0d5cb72cfb
PhanHongHuynh <huynhph141100@gmail.com>
2022-07-22 16:19:57 +0700

    migration

--- database/migrations/2022_07_22_034705_create_roles_permissions_table.php
+++ database/migrations/2022_07_22_034705_create_roles_permissions_table.php
@@ -13,14 +13,12 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+        Schema::create('roles_permissions', function (Blueprint $table) {
+            $table->integer('permission_id');
+            $table->integer('role_id');
             $table->timestamps();
+
+
         });
     }
 
@@ -31,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('roles-permissions');
     }
 };
