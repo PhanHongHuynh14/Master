edbf06fda0cde3661a67e5a4324bbfe8592a8343
PhanHongHuynh <huynhph141100@gmail.com>
2022-08-12 11:24:58 +0700

    Language

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,6 +64,5 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'verifyadmin' => \App\Http\Middleware\VerifyAdminMiddleware::class,
-        'locale' => \App\Http\Middleware\LangMiddleware::class,
     ];
 }
